2860adf1b0c235784051a142cbd194f00bd51eaf
Mateus Pincho <mateus.oliveira@ee.ufcg.edu.br>
2025-01-29 12:27:41 -0300

    creating notebook for better understanding

--- manipuladores/cinematic.py
+++ manipuladores/cinematic.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python3
 import roboticstoolbox as rtb
 
 # import the UR10 model from the toolbox
-robot = rtb.models.DH.UR10()
-print(robot)
+UR10 = rtb.models.URDF.UR5()
+
+UR10.fkine(UR10.qr).printline()
+
+UR10.plot(UR10.qr)
